ff4a7300a64fb178ec6b8d4df12ea1a0d5aa7599
jaredatandi <jaredatandi07@gmail.com>
2023-07-01 12:34:10 +0300

    Create a report object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeeList) {
+  return {
+    allEmployees: {
+      ...employeeList,
+    },
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
+  };
+}
