c4602ca40ad6043af2df9e3d52b3f60fe5ebbfed
jaredatandi <jaredatandi07@gmail.com>
2023-07-03 16:27:36 +0300

    use await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let results = {};
+
+  try {
+    const pic = await uploadPhoto();
+    const user = await createUser();
+
+    results = { pic, user };
+  } catch (err) {
+    results = { photo: null, user: null };
+  }
+  return results;
+}
