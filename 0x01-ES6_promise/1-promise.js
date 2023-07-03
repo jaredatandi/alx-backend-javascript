042561a7edcf113bf091e80f87026a990c36c483
jaredatandi <jaredatandi07@gmail.com>
2023-07-03 15:04:04 +0300

    Remove Error

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-  return Promise(((resolve, reject) => {
-    if (success === true) {
+  return new Promise(((resolve, reject) => {
+    if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
-      reject(Error('The fake API is not working currently'));
+      reject(new Error('The fake API is not working currently'));
     }
   }));
 }
